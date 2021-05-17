6afbc4250697abbcea1f911f32566ddbdc094ba4
Joao Pedro Portela <porthunt@Joaos-MacBook-Air.local>
2021-05-17 12:39:35 +0200

    Passing SG name and port as bash parameter

--- addip.sh
+++ addip.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt > /dev/null
 ip_address="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-python manage_sg.py --add-ip ${ip_address}/32 --sg-name awseb-e-dzmgkpajs5-stack-AWSEBLoadBalancerSecurityGroup-H1S7N1WWUKT9 --port 80
+python manage_sg.py --add-ip ${ip_address}/32 --sg-name $1 --port $2
